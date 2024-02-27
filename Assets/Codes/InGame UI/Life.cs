faa00b0bec3b52f4270178a7b2d5dae445320457
choiJhoon <jhoon0929@gmail.com>
2024-02-27 19:17:00 +0900

    생명 시스템 및 에셋 파일

--- Assets/Codes/InGame UI/Life.cs
+++ Assets/Codes/InGame UI/Life.cs
@@ -6,5 +6,23 @@ using System.Resources;
 
 public class Life : MonoBehaviour
 {
-    
+    private Player player;
+    public TextMeshProUGUI healthText;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        // Life 스크립트가 부착된 GameObject에 연결된 Player 스크립트를 찾음
+
+        if (Player.Instance.health == null)
+        {
+            Debug.LogError("Player 스크립트를 찾을 수 없습니다.");
+        }
+    }
+
+    void Update()
+    {
+        // 이후 PlayerHP 변수를 사용할 수 있음
+        healthText.text = player.health.ToString();
+    }
 }
