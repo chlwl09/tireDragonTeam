4ce25cb1fad955776206a19c26738a10c11d0645
choiJhoon <jhoon0929@gmail.com>
2024-02-27 13:20:38 +0900

    캐릭터 움직임 구현

--- Assets/Codes/Player.cs
+++ Assets/Codes/Player.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public 
 
+    public Vector2 inputVec;
+    public float MoveSpeed = 1.0f;
 
-    void Start()
+    Rigidbody2D rigid;
+    private void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
-    
 
     void Update()
     {
-        
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 nextVc = inputVec * MoveSpeed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVc);
     }
 }
